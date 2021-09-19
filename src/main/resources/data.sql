f661644d75511c532083f936afc71630925f0ef9
prajjawaltiwari <prajjawaltiwari1811@gmail.com>
2021-09-19 23:27:25 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-insert into user (userid,upassword,uname,role) values ('admin','admin','admin','admin');
-insert into user (userid,upassword,uname,role) values ('admin1','admin1','admin1','user');
+insert into user (userid,upassword,uname,role) values ('admin1@gmail.com','admin','admin','admin');
+insert into user (userid,upassword,uname,role) values ('user@gmail.com','admin1','user1','user');
+insert into user (userid,upassword,uname,role) values ('admin@gmail.com','admin1','admin1','admin');
+insert into user (userid,upassword,uname,role) values ('user1@gmail.com','admin1','user2','user');
+insert into user (userid,upassword,uname,role) values ('admin2@gmail.com','admin1','admin3','admin');
